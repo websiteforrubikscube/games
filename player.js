52634f1e5991b21ce3bbeda59ce3af8c5ba117e8
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-20 10:09:11 -0400

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,13 @@
 
+export default class Player{
+  constructor(){
+    this.width = 100;
+    this.height = 150;
+    this.position ={x:0, y:350};
+  }
+  draw(ctx){
+    ctx.fillStyle='#FFA500';
+    ctx.fillRect(this.position.x, this.position.y,
+      this.width, this.height);
+  }
+}
