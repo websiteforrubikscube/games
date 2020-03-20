71948a52fe2a89efccf8388af7f688b64b702906
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-20 10:10:40 -0400

    Update level1.js

--- level1.js
+++ level1.js
@@ -1,11 +1,14 @@
  import Ground from "./ground.js";
+import Player from "./player.js";
 
 export default function launch(){
   let canv = document.getElementById("Screen");
   let ctx = canv.getContext('2d');
 
  let ground = new Ground();
+ let player = new Player();
 ground.draw(ctx);
+ player.draw(ctx);
  
 
 }
