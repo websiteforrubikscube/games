f2ce3a6ea49fa0210897c64c98cd1efb9e1fc253
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-20 09:59:24 -0400

    Update ground.js

--- ground.js
+++ ground.js
@@ -4,10 +4,10 @@ export default class Ground{
   constructor(){
     this.width = 1000;
     this.height = 50;
-    this.position ={x:50, y:50};
+    this.position ={x:400, y:0};
   }
   draw(ctx){
-    ctx.fillStyle='#F00';
+    ctx.fillStyle='#00800';
     ctx.fillRect(this.position.x, this.position.y,
       this.width, this.height);
   }
