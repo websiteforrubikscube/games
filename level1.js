a703fd1b36e3160803f5675ab1d895e358b17354
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-20 09:32:40 -0400

    Update level1.js

--- level1.js
+++ level1.js
@@ -1,10 +1,9 @@
- 
+ import Ground from "ground.js";
 
 export default function launch(){
   let canv = document.getElementById("Screen");
   let ctx = canv.getContext('2d');
   ctx.fillStyle='#F00';
-var ground = require("ground.js");
  ground.draw(ctx);
 }
 
