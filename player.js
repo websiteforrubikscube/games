b47683cd25f4142521aba593f929921fe5582d84
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-20 13:47:51 -0400

    Update player.js

--- player.js
+++ player.js
@@ -3,11 +3,14 @@ export default class Player{
   constructor(runnumber){
     this.width = 100;
     this.height = 150;
-    this.position ={x:runnumber * 10, y:250};
+    this.position ={x:0, y:250};
   }
   draw(ctx){
     ctx.fillStyle='#FFA500';
     ctx.fillRect(this.position.x, this.position.y,
       this.width, this.height);
+  }
+  function proccessloop(runnumber){
+  this.position.x=runnumber;
   }
 }
