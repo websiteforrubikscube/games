becb7e5393e75f5520ef28b21f7a25a86d6e99d8
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-20 20:15:30 -0400

    Update player.js

--- player.js
+++ player.js
@@ -5,10 +5,10 @@ export default class Player{
     this.height = 150;
     this.position ={x:0, y:250};
     this.keys = {};
-    this.keys[37]=false;
-    this.keys[38]=false;
-    this.keys[39]=false;
-    this.keys[40]=false;
+    this.keys[37]={};
+    this.keys[38]={};
+    this.keys[39]={};
+    this.keys[40]={};
     window.onkeyup = function(e) { this.keys[e.keyCode] = false; }
     window.onkeydown = function(e) { this.keys[e.keyCode] = true; }   
   }
