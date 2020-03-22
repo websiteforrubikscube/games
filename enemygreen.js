014ad7b1aa505031ec79a994680c8a4c40e77c4a
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-22 07:19:39 -0400

    Update enemygreen.js

--- enemygreen.js
+++ enemygreen.js
@@ -7,7 +7,7 @@ this.width=200;
   this.position={x:400, y:300};
 }
 draw(ctx){
-ctx.fillStyle=('#90EE90');
+ctx.fillStyle=(color);
   ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
 }
 proccessloop(ctx){
@@ -17,8 +17,10 @@ this.position.x= this.position.x + 5;
   if((window.PlayerX-50<this.position.x)&&(window.PlayerY>249)){
 this.position.x= this.position.x - 5;
 }
-  if((window.PlayerX>this.position.x)&&(window.PlayerX<this.position.x+200)&&(window.PlayerY=300)){
-  ctx.fillStyle=("#d9ffdd");
+  if((window.PlayerX>this.position.x-100)&&(window.PlayerX<this.position.x+200)&&(window.PlayerY=300)){
+ var color =("#d9ffdd");
+  }else{
+  var color  =('#90EE90');
   }
   if(this.position.x<0){
   this.position.x=0;
