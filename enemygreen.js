6f177b37d3c006bb11e2518ae30fcda11ae2304b
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-22 07:32:02 -0400

    Update enemygreen.js

--- enemygreen.js
+++ enemygreen.js
@@ -19,7 +19,7 @@ this.position.x= this.position.x + 5;
 this.position.x= this.position.x - 5;
 }
   if((window.PlayerX>this.position.x-100)&&(window.PlayerX<this.position.x+200)){
-    if(window.PlayerY>501){
+    if(window.PlayerY>101){
  window.color =("#d9ffdd");
     }
   }else{
