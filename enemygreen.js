dee8479bbcfe8fec7e85815f1de46cc8ba01716d
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-22 10:31:52 -0400

    Update enemygreen.js

--- enemygreen.js
+++ enemygreen.js
@@ -12,6 +12,8 @@ draw(ctx){
   if(this.visable==true){
 ctx.fillStyle=(window.color);
   ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }else{
+  this.visable=(false);
   }
   }
 proccessloop(ctx){
