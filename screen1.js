1f9b38c474fe94c74f792297f39ed50dee26ebcc
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-24 16:53:00 -0400

    Update screen1.js

--- screen1.js
+++ screen1.js
@@ -7,6 +7,7 @@ this.height=500;
 this.position={x:950, y:0};
  window.screen1=(true);
  window.screen2=(false);
+ this.move=(true)
 }
 draw(ctx){
 ctx.fillStyle=("#000");
@@ -16,6 +17,8 @@ ctx.fillRect(this.position.x,this.position.y, this.width, this.height);
  if((window.PlayerX+100)>this.position.x){
  window.screen1=(false);
   window.screen2=(true);
+  this.move=(false);
+  setTimeout(function(move){ move.move=(true); }, 1,this);
  }
  }
 }
