14cc5307703f43aad573b7f250de15be87349b50
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-24 13:10:52 -0400

    Create teleport.js

---
+++ teleport.js
@@ -0,0 +1,13 @@
+import Player from "./player.js";
+
+class Teleport{
+constructor(){
+this.width=50;
+this.height=500;
+this.position={x:950, y:0};
+}
+draw(ctx){
+ctx.fillStyle=("#000");
+ctx.fillRect=(this.position.x,this.position.y, this.width, this.height);
+}
+}
