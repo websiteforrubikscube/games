85e4260d94ad1f521c26a4f7277f3c9faba3cdb3
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-24 21:20:02 -0400

    Update enemyyellow.js

--- enemyyellow.js
+++ enemyyellow.js
@@ -17,12 +17,13 @@ this.position.x=this.position.x+10;
     if((window.PlayerX<this.position.x+25)&&(window.PlayerY>249)){
 this.position.x=this.position.x-10;
 }
-    
+    if(window.PlayerY<249){
     if(window.PlayerX-this.position.x-25<0){
     this.position.x=this.position.x+5;
     }
       if(window.PlayerX-this.position.x-25>0){
     this.position.x=this.position.x-5;
+    }
     }
     
     if((window.PlayerX>this.position.x-100)&&(window.PlayerX<this.position.x+150)){
