f73099563869119b87697ec4e0a9ebb8ae708381
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-24 21:24:23 -0400

    Update enemyyellow.js

--- enemyyellow.js
+++ enemyyellow.js
@@ -11,10 +11,10 @@ ctx.fillStyle=("#ffed26");
 ctx.fillRect(this.position.x, this.position.y,this.width,this.height)
 }
   proccessloop(ctx){
-if((window.PlayerX>this.position.x+15)&&(window.PlayerY>249)){
+if((window.PlayerX>this.position.x+25)&&(window.PlayerY>249)){
 this.position.x=this.position.x+10;
 }
-    if((window.PlayerX<this.position.x+15)&&(window.PlayerY>249)){
+    if((window.PlayerX<this.position.x+25)&&(window.PlayerY>249)){
 this.position.x=this.position.x-10;
 }
     if(window.PlayerY<249){
