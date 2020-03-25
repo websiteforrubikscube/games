46c5c2fa7e6ee883738a9f542fbeb911d61572c2
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-24 21:27:12 -0400

    Update enemyyellow.js

--- enemyyellow.js
+++ enemyyellow.js
@@ -5,10 +5,13 @@ constructor(){
 this.width=150;
 this.height=100;
 this.position={x:425,y:300};
+  this.visable=(true);
 }
 draw(ctx){
+  if(this.visable==(true)){
 ctx.fillStyle=("#ffed26");
-ctx.fillRect(this.position.x, this.position.y,this.width,this.height)
+ctx.fillRect(this.position.x, this.position.y,this.width,this.height);
+  }
 }
   proccessloop(ctx){
 if((window.PlayerX>this.position.x+25)&&(window.PlayerY>249)){
@@ -28,7 +31,7 @@ this.position.x=this.position.x-10;
     
     if((window.PlayerX>this.position.x-100)&&(window.PlayerX<this.position.x+150)){
     if((window.PlayerY>this.position.y-151)&&(window.PlayerY+150<this.position.y+11)){
-    console.log("hello");
+   ctx.fillStyle=("#fff58a");
     }
     }
   if(this.position.x<0){
