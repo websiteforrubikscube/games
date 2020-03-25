f719dedcc19e793552f5821e58278cd748edb54d
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-25 09:30:28 -0400

    Update screen2.js

--- screen2.js
+++ screen2.js
@@ -5,13 +5,15 @@ this.height=500;
 this.position={x:950,y:0};
 }
 draw(ctx){
-ctx.fillStyle=("#000");
+ctx.fillStyle=("#87ceeb");
 ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
 }
 proccessloop(){
 if(window.PlayerX+100>950){
 window.screen2=(false);
   window.screen3=(true);
+  this.move=(false);
+ setTimeout(function(){ this.move=(true); }, 34);
 }
 }
 }
