f81a6338b8de7bdc10eade2a83f11ac1b2f8882f
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-24 21:49:54 -0400

    Update player.js

--- player.js
+++ player.js
@@ -69,6 +69,21 @@ proccessloop(ctx){
     }
   }
   }
+ if((this.position.y+150)>(window.enemy2Y-40)){
+  if((this.position.x+100)>(window.enemy2X)){
+    if((this.position.x)<(window.enemy2X+150)){
+         if((window.visable==true)&&(window.invisabledamage==(false))){
+           this.living=(false);
+    window.color1=("#ffd5a1");
+           keys[37]=(false);
+            keys[38]=(false);
+            keys[39]=(false);
+            keys[40]=(false);
+              setTimeout(function(player){ player.visable=(false); }, 1000,this);
+         }
+    }
+  }
+  }
   if(window.screen2==(true)){
    if(window.move == (false)){
     if(this.living == (true)){
