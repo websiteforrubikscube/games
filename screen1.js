dcd7d9c4b01f0b91e1518097888279a98216b1fa
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-25 09:39:28 -0400

    Update screen1.js

--- screen1.js
+++ screen1.js
@@ -14,12 +14,14 @@ ctx.fillStyle=("#87CEEB");
 ctx.fillRect(this.position.x,this.position.y, this.width, this.height);
 }
  proccessloop(){
+  if(window.screen1==(true)){
  if((window.PlayerX+100)>this.position.x){
  window.screen1=(false);
   window.screen2=(true);
   this.move=(false);
  setTimeout(function(){ this.move=(true); }, 34);
  }
+  }
   window.move=this.move; 
  }
 }
