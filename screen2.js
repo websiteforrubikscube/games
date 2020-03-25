6a5ee4d6a51160332c3c882ba7ce0377675cd73e
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-25 09:24:40 -0400

    Create screen2.js

---
+++ screen2.js
@@ -0,0 +1,16 @@
+export default Screen2{
+constructor(){
+this.width=50;
+this.height=500;
+this.position={x:950,y:0};
+}
+draw(ctx){
+ctx.fillStyle=("#000");
+ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
+}
+proccessloop(){
+if(window.PlayerX+100>950){
+
+}
+}
+}
