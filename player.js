d53cc8964b3797568448966c9484845e6d25ba64
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-26 08:11:24 -0400

    Update player.js

--- player.js
+++ player.js
@@ -50,11 +50,11 @@ proccessloop(ctx){
            }
     }
    if ((keys[38]==true)&&(this.living==true)){
-       speed=speed+1;
-      this.position.y = this.position.y + speed;
+      this.position.y=this.position.y+1;
     }
-  if ((keys[40]==true)&&(this.living==true)){
-      this.position.y = this.position.y + 5;
+  if(this.position.y<250){
+    speed=speed+1;
+      this.position.y = this.position.y + speed;
   }
   if((this.position.y+150)>(window.enemy1c-40)){
   if((this.position.x+100)>(window.enemy1c2-100)){
