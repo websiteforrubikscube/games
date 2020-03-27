104916472c335ef9f732f9c27cdd9fa01b6c147d
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-27 10:41:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ import Level2 from "./level2.js";
 let level1 = new Level1();
 let level2 = new Level2();
 
+window.screen1=(true);
 export default function launch(){
+  if(window.screen1==(true)){
 level1.proccess();
-  if(window.screen4=(true)){
+  }
+  if(window.screen4==(true)){
 level2.proccess();
   }
 }
