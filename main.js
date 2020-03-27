18c501476da644e41358cdec273d9cefb3b23e6b
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-27 09:44:04 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+import Level1 from "./level1.js";
+import Level2 from "./level2.js";
+
+let level1 = new Level1();
+let level2 = new Level2();
+
+export default function launch(){
+level1.proccess();
+level2.proccess();
+}
