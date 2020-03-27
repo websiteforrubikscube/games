e04848cb19deeb0294d4f81e6d02844bd1ce2eb0
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-27 11:25:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ import Level2 from "./level2.js";
 let level1 = new Level1();
 let level2 = new Level2();
 
-window.screen1=(true);
+window.start=(true);
 export default function launch(){
   gameloop();
   function gameloop(){
-  if(window.screen1==(true)){
+  if(window.start==(true)){
 level1.proccess();
   }
   if(window.screen4==(true)){
+    window.start=(false);
 level2.proccess();
   }
     window.setTimeout(function() {gameloop();}, 33);
