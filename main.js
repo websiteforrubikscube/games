ac46425607d41bd133deb450f6cfa86ae3786e5b
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-27 10:47:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,15 @@ let level2 = new Level2();
 
 window.screen1=(true);
 export default function launch(){
+  screencheck();
+  function screencheck(){
   if(window.screen1==(true)){
 level1.proccess();
   }
   if(window.screen4==(true)){
 level2.proccess();
   }
+  window.setTimeout(function() {screencheck();}, 33);
+   }
 }
 window.launch=launch;
