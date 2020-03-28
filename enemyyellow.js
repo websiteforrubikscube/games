38ad3d976b9ce1eb919d555e35a6f759292a7a8a
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-28 08:31:13 -0400

    Update enemyyellow.js

--- enemyyellow.js
+++ enemyyellow.js
@@ -84,13 +84,111 @@ this.position.x=this.position.x-10;
     this.position.y=300;
   }
     if(screen4load==(true)){
-    window.invisabledamage1=(true);
-      this.visable=(false);
-      this.hit=2;
+      this.living=(true);
+    window.invisabledamage1=(false);
+      this.visable=(true);
+      this.hit=0;
+      this.immune=(false);
+        window.color2=("#fce700");
+      this.position.x=550;
     }
     window.enemy2X=this.position.x;
     window.enemy2Y=this.position.y;
     window.visable1=this.visable;
+  }
+
+}
+
+export default class Enemyyellow2{
+constructor(){
+this.width=150;
+this.height=100;
+this.position={x:425,y:300};
+  this.visable=(true);
+  window.invisabledamage2=(false);
+  window.color3=("#fce700");
+  this.hit=0;
+  this.immune=(false);
+  this.living=(true);
+}
+draw(ctx){
+  if(this.visable==(true)){
+ctx.fillStyle=(window.color2);
+ctx.fillRect(this.position.x, this.position.y,this.width,this.height);
+  }
+}
+  proccessloop(ctx){
+    //movement
+if((window.PlayerX>this.position.x+25)&&(window.PlayerY>249)){
+this.position.x=this.position.x+10;
+}
+    if((window.PlayerX<this.position.x+25)&&(window.PlayerY>249)){
+this.position.x=this.position.x-10;
+}
+    if(window.PlayerY<249){
+    if(window.PlayerX-this.position.x-25<0){
+    this.position.x=this.position.x+5;
+    }
+      if(window.PlayerX-this.position.x-25>0){
+    this.position.x=this.position.x-5;
+    }
+    }
+    //death
+    if((window.PlayerX>this.position.x-100)&&(window.PlayerX<this.position.x+150)){
+    if((window.PlayerY>this.position.y-151)&&(window.PlayerY+150<this.position.y+11)){
+      if(window.screenload==(false)){
+      if(this.immune==(false)){
+      this.hit=this.hit+1;
+      if(this.hit>1){
+        this.living=(false);
+       window.color3=("#fdff9e");
+        window.invisabledamage2=(true);
+        setTimeout(function(enemy){ enemy.visable=(false); }, 1000,this);
+      }
+        if(this.hit<2){
+         this.immune = (true);
+   window.color3=("#fdff9e");
+      window.invisabledamage2=(true);
+      setTimeout(function(enemy){ enemy.immune=(false); }, 1000,this);
+      setTimeout(function(enemy){ enemy.invisabledamage2=(false); }, 1000,window);
+      setTimeout(function(enemy){ enemy.color3=("#fce700"); }, 1000,window);
+        }
+        }
+    }
+    }
+    }
+    if(this.hit==0){
+    this.living=(true);
+    }
+    if(this.living==(true)){
+    this.visable=(true);
+    }
+  if(this.position.x<0){
+  this.position.x=0;
+  }
+  if(this.position.x>850){
+  this.position.x=850;
+  }
+    if(window.screen2load==(true)){
+  this.visable=(false);
+      this.living=(false);
+    }
+    if(window.screen3load==(true)){
+      this.visable=(false);
+      this.living=(false);
+  }
+    if(screen4load==(true)){
+    this.living=(true);
+    window.invisabledamage2=(false);
+      this.visable=(true);
+      this.hit=0;
+      this.immune=(false);
+        window.color3=("#fce700");
+      this.position.x=300;
+    }
+    window.enemy2X2=this.position.x;
+    window.enemy2Y2=this.position.y;
+    window.visabl2=this.visable;
   }
 
 }
