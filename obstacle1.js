f00a723fa91c2ec74c0eb1db32b39563eb227c7f
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-30 08:49:21 -0400

    Create obstacle1.js

---
+++ obstacle1.js
@@ -0,0 +1,19 @@
+export default class Obstacle1{
+contructor(){
+this.height=100;
+this.width=100;
+this.position={x:450,y:300};
+}
+draw(ctx){
+ctx.fillStyle=("");
+ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+}
+proccessloop(){
+if((window.PlayerY+151>this.position.y)&&(window.PlayerX+150>this.position.x)){
+if(window.PlayerX<550){
+window.touch=(true);
+}
+}
+}
+
+}
