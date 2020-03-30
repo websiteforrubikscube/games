068483bd76b5127fc8c093b3706f3514e251efc3
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-30 07:24:24 -0400

    Create screen4.js

---
+++ screen4.js
@@ -0,0 +1,25 @@
+export default class Screen4{
+constructor(){
+this.width=50;
+this.height=500;
+this.position={x:950, y:0};
+ window.screen4=(true);
+ window.screen5=(false);
+ this.move=(true)
+}
+draw(ctx){
+ctx.fillStyle=("#87CEEB");
+ctx.fillRect(this.position.x,this.position.y, this.width, this.height);
+}
+ proccessloop(){
+  if(window.screen4==(true)){
+ if((window.PlayerX+100)>this.position.x){
+ window.screen4=(false);
+  window.screen5=(true);
+  this.move=(false);
+ setTimeout(function(){ this.move=(true); }, 34);
+ }
+  }
+  window.move=this.move; 
+ }
+}
