35a7cbc6c8d8d45600f10276bae80a1475a67843
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-03-30 10:54:53 -0400

    Update obstacle1.js

--- obstacle1.js
+++ obstacle1.js
@@ -3,6 +3,9 @@ constructor(){
 this.height=100;
 this.width=100;
 this.position={x:450,y:300};
+  window.touchX1=(false);
+  window.touchX2=(false);
+ window.touchY=(false);
 }
 draw(ctx){
 ctx.fillStyle=("#008000");
@@ -12,7 +15,12 @@ proccessloop(){
 if((window.PlayerY+151>this.position.y)&&(window.PlayerX+150>this.position.x)){
 if(window.PlayerX<550){
 window.touch=(true);
+  window.touchY=(true);
+}else{
+window.touchY=(false)
 }
+}else{
+window.touchY=(false)
 }
 }
 
