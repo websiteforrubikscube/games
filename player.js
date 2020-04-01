bafc385116d23bee66278c9cf4423f506f8964d1
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-04-01 10:52:49 -0400

    Update player.js

--- player.js
+++ player.js
@@ -77,8 +77,8 @@ proccessloop(ctx){
   if((window.touchY==true)&&(keys[38]==false)){
   this.position.y=window.obstacle1X-299;
   }
-  if(this.speed>10){
-  this.speed=10;
+  if(this.speed>15){
+  this.speed=15;
   }
   //death
   if((this.position.y+150)>(window.enemy1c-40)){
