ae8cfc441f283641661b6042a43bfd7db2c79a56
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-04-03 11:15:37 -0400

    Update obstacle2.js

--- obstacle2.js
+++ obstacle2.js
@@ -3,9 +3,9 @@ constructor(){
 this.height=100;
 this.width=100;
 this.position={x:600,y:300};
-  window.touchX1=(false);
-  window.touchX2=(false);
- window.touchY=(false);
+  window.touchX12=(false);
+  window.touchX22=(false);
+ window.touchY2=(false);
 }
 draw(ctx){
 ctx.fillStyle=("#008000");
@@ -14,36 +14,36 @@ ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
 proccessloop(){
 if((window.PlayerY+151>this.position.y)&&(window.PlayerX+100>this.position.x)){
 if((window.PlayerX<this.position.x+100)&&(window.PlayerY+151<this.position.y+16)){
-window.touch=(true);
-  window.touchY=(true);
+window.touch2=(true);
+  window.touchY2=(true);
 }else{
-window.touchY=(false);
-  window.touch=(false);
+window.touchY2=(false);
+  window.touch2=(false);
 }
 }else{
-window.touchY=(false);
-  window.touch=(false);
+window.touchY2=(false);
+  window.touch2=(false);
 }
   if((window.PlayerX+100>this.position.x)&&(window.PlayerX+50<this.position.x+50)){
 if(window.PlayerY+150>this.position.y+10){
-window.touchX1=(true);
-}else{window.touchX1=(false);}
-  }else{window.touchX1=(false);}
+window.touchX12=(true);
+}else{window.touchX12=(false);}
+  }else{window.touchX12=(false);}
    if((window.PlayerX<this.position.x+100)&&(window.PlayerX+50>this.position.x+50)){
 if(window.PlayerY+150>this.position.y+10){
-window.touchX2=(true);
-}else{window.touchX2=(false);}
-  }else{window.touchX2=(false);}
+window.touchX22=(true);
+}else{window.touchX22=(false);}
+  }else{window.touchX22=(false);}
   if((window.enemy2X+150>this.position.x)&&(enemy2X+75<this.position.x+50)){
   if(window.enemy2Y+50>this.position.y){
-  window.enemy2Xtouch1=(true);
-  }else{ window.enemy2Xtouch1=(false);}
-  }else{ window.enemy2Xtouch1=(false);}
+  window.enemy2Xtouch12=(true);
+  }else{ window.enemy2Xtouch12=(false);}
+  }else{ window.enemy2Xtouch12=(false);}
   if((window.enemy2X<this.position.x+100)&&(window.enemy2X+75>this.position.x+50)){
   if(window.enemy2Y+50>this.position.y){
-window.enemy2Xtouch2=(true);
-  }else{ window.enemy2Xtouch2=(false);}
-  }else{ window.enemy2Xtouch2=(false);}
+window.enemy2Xtouch22=(true);
+  }else{ window.enemy2Xtouch22=(false);}
+  }else{ window.enemy2Xtouch22=(false);}
   window.obstacle2Y=this.position.y;
   window.obstacle2X=this.position.x;
 }
