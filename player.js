48467bb5d12c97457daa70c0871366b18113507a
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-04-03 10:29:48 -0400

    Update player.js

--- player.js
+++ player.js
@@ -75,7 +75,7 @@ proccessloop(ctx){
    this.position.y=250; 
   }
   if((window.touchY==true)&&(keys[38]==false)){
-  this.position.y=window.obstacle1Y-249;
+  this.position.y=window.obstacle1Y-149;
   }
   if(this.speed>15){
   this.speed=15;
