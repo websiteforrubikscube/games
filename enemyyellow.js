8750aed1ed99596c361a3ac9bacd92020af8c52c
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-04-03 11:56:36 -0400

    Update enemyyellow.js

--- enemyyellow.js
+++ enemyyellow.js
@@ -11,6 +11,10 @@ this.position={x:425,y:300};
   this.hit=0;
   this.immune=(false);
   this.living=(true);
+  this.active1=(false);
+  this.active2=(false);
+  this.active3=(false);
+  this.active4=(false);
 }
 draw(ctx){
   if(this.visable==(true)){
@@ -20,62 +24,94 @@ ctx.fillRect(this.position.x, this.position.y,this.width,this.height);
 }
   proccessloop(ctx){
     //movement
+    if(this.active1==(false)){
 if((window.PlayerX>this.position.x+25)){
     if((window.touch==true)||(window.PlayerY>249)){
     if((window.enemy2Xtouch1==(false))&&(window.enemy2Xtouch12==(false))){
 this.position.x=this.position.x+10;
-    }
-}
-}
+      this.active1=(true);
+    }else{this.active1=(false);}
+}else{this.active1=(false);}
+}else{this.active1=(false);}
+    }else{this.active1=(false);}
+    
+ if(this.active1==(false)){
     if((window.PlayerX>this.position.x+25)){
     if((window.touch2==true)||(window.PlayerY>249)){
     if((window.enemy2Xtouch1==(false))&&(window.enemy2Xtouch12==(false))){
 this.position.x=this.position.x+10;
-    }
-}
-}
+       this.active1=(true);
+    }else{this.active1=(false);}
+}else{this.active1=(false);}
+}else{this.active1=(false);}
+ }else{this.active1=(false);}
+    
+     if(this.active2==(false)){
     if(window.PlayerX<this.position.x+25){
       if((window.touch==true)||(window.PlayerY>249)){
    if((window.enemy2Xtouch2==(false))&&(window.enemy2Xtouch22==(false))){
 this.position.x=this.position.x-10;
-   }
-}
-    }
+     this.active2=(true);
+   }else{this.active2=(false);}
+}else{this.active2=(false);}
+    }else{this.active2=(false);}
+  }else{this.active2=(false);}
+    
+     if(this.active2==(false)){
      if(window.PlayerX<this.position.x+25){
       if((window.touch2==true)||(window.PlayerY>249)){
    if((window.enemy2Xtouch2==(false))&&(window.enemy2Xtouch22==(false))){
 this.position.x=this.position.x-10;
-   }
-}
-    }
+     this.active2=(true);
+  }else{this.active2=(false);}
+}else{this.active2=(false);}
+    }else{this.active2=(false);}
+  }else{this.active2=(false);}
+
+      if(this.active3==(false)){
     if((window.PlayerY<249)&&(window.touch==(false))){
     if(window.PlayerX-this.position.x-25<0){
         if((window.enemy2Xtouch1==(false))&&(window.enemy2Xtouch12==(false))){
     this.position.x=this.position.x+5;
-        }
-    }
-    }
+      this.active3=(true);
+  }else{this.active3=(false);}
+}else{this.active3=(false);}
+    }else{this.active3=(false);}
+  }else{this.active3=(false);}
+
+      if(this.active3==(false)){
        if((window.PlayerY<249)&&(window.touch2==(false))){
     if(window.PlayerX-this.position.x-25<0){
         if((window.enemy2Xtouch1==(false))&&(window.enemy2Xtouch12==(false))){
     this.position.x=this.position.x+5;
-        }
-    }
-       }
+      this.active3=(true);
+  }else{this.active3=(false);}
+}else{this.active3=(false);}
+    }else{this.active3=(false);}
+  }else{this.active3=(false);}
+
+     if(this.active4==(false)){
          if((window.PlayerY<249)&&(window.touch==(false))){
       if(window.PlayerX-this.position.x-25>0){
           if((window.enemy2Xtouch2==(false))&&(window.enemy2Xtouch22==(false))){
     this.position.x=this.position.x-5;
-          }
-    }
-    }
+          this.active4=(true);
+  }else{this.active4=(false);}
+}else{this.active4=(false);}
+    }else{this.active4=(false);}
+  }else{this.active4=(false);}
+
+    if(this.active4==(false)){
            if((window.PlayerY<249)&&(window.touch2==(false))){
       if(window.PlayerX-this.position.x-25>0){
           if((window.enemy2Xtouch2==(false))&&(window.enemy2Xtouch22==(false))){
     this.position.x=this.position.x-5;
-          }
-    }
-    }
+        this.active4=(true);
+  }else{this.active4=(false);}
+}else{this.active4=(false);}
+    }else{this.active4=(false);}
+  }else{this.active4=(false);}
+
     //death
     if((window.PlayerX>this.position.x-100)&&(window.PlayerX<this.position.x+150)){
     if((window.PlayerY>this.position.y-151)&&(window.PlayerY+150<this.position.y+11)){
