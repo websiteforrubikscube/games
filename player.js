bc35b85da53b2c9fc1245da33cd8a925f799d611
websiteforrubikscube <60789077+websiteforrubikscube@users.noreply.github.com>
2020-04-04 10:04:17 -0400

    Update player.js

--- player.js
+++ player.js
@@ -93,7 +93,19 @@ proccessloop(ctx){
   this.speed=15;
   }
   
-  if(window.killjump==(true)){
+   if(window.killjump3==true){
+  if(this.speed>0){
+  this.speed=this.speed*-0.75;
+  }
+    if(this.speed==0){
+    this.speed=-10;
+    }
+    if(this.speed<0){
+    this.speed=this.speed*1.5
+    }
+  }
+  
+  if((window.killjump==true)&&(window.killjump2==true)){
   if(this.speed>0){
   this.speed=this.speed*-0.75;
   }
