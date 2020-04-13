573ddb734d0cb660c35d248eef57b16aeefcabc6
websiteforrubikscube <mineyuri77@gmail.com>
2020-04-13 18:36:21 -0400

    k

--- screen4.js
+++ screen4.js
@@ -16,7 +16,7 @@ ctx.fillRect(this.position.x,this.position.y, this.width, this.height);
  window.screen4=(false);
   window.screen5=(true);
   this.move=(false);
- setTimeout(function(load){ load.move=(true); }, 34, load);
+ setTimeout(function(load){ load.move=(true); }, 34,this);
  }
   }
   window.move=this.move; 
