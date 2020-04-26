f7d82cfbfe319787b7e9f6fd2417cf5d504d192f
websiteforrubikscube <mineyuri77@gmail.com>
2020-04-26 10:13:43 -0400

    k

---
+++ screen8.js
@@ -0,0 +1,27 @@
+export default class Screen8{
+    constructor(){
+    this.width=50;
+    this.height=500;
+    this.position={x:950, y:0};
+     window.screen8=(false);
+     this.move=(true);
+     this.redenemy1dead=false;
+    }
+    draw(ctx){
+    ctx.fillStyle=("#87CEEB");
+    ctx.fillRect(this.position.x,this.position.y, this.width, this.height);
+    }
+     proccessloop(){
+      if(window.screen8==(true)){
+     if((window.PlayerX+100)>this.position.x){
+        if((this.redenemy1dead==true)&&(this.redenemy2dead==true)){
+     window.screen8=(false);
+      window.screen9=(true);
+      this.move=(false);
+     setTimeout(function(load2){ load2.move=(true); }, 34, this);
+     }
+      }
+    }
+      window.move=this.move; 
+     }
+    }
