e3ec31a438a279cfc9315822181647af3f252c1d
websiteforrubikscube <mineyuri77@gmail.com>
2020-05-01 09:18:12 -0400

    k

--- player.js
+++ player.js
@@ -141,7 +141,8 @@ export default class Player {
         this.killjumpifskip=true;
       }
     }
-    }else{ setTimeout(function (player) { player.killjumpifskip = (false); }, 100, this);}
+    }
+    else{ setTimeout(function (player) { player.killjumpifskip = (false); }, 1000, this);}
 
     //death
     if ((this.position.y + 150) > (window.enemy1c - 33)) {
