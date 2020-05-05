43413fcc5f18afc66ec62a6a54acf00e7de41d56
websiteforrubikscube <mineyuri77@gmail.com>
2020-05-05 12:06:37 -0400

    k

---
+++ level4.js
@@ -0,0 +1,71 @@
+import Ground from "./ground.js";
+import Player from "./player.js";
+import Sky from "./sky.js";
+
+
+let ground = new Ground();
+let player = new Player();
+ let sky = new Sky();
+
+
+var runnumber14=0;
+var runnumber24=0;
+var runnumber34=0;
+
+export default class Level4{
+ 
+    constructor (){
+      this.ctx=undefined;
+      this.respawn = (false);
+      window.touch=false;
+      window.touch2=false;
+      }
+  
+   proccess4(){
+    let canv = document.getElementById("Screen");
+    this.ctx = canv.getContext('2d');
+    
+    if (player.visable==false){
+      this.respawnmain();
+      return;
+    }
+    this.erraseCanvas();
+    this.draw();
+ 
+    runnumber14++;
+    if(window.screen10==true){
+        ground.proccessloop(this.ctx);
+     player.proccessloop(this.ctx);
+     if(runnumber14<2){
+         window.screenload=true;
+         window.screen10load=true;
+         setTimeout(function(load){ load.screenload=(false); }, 34,window);
+         setTimeout(function(load){ load.screen10load=(false); }, 34,window);
+     }
+    }
+}
+
+
+respawnmain(){
+ runnumber14=0;
+ runnumber24=0;
+ runnumber34=0;
+ player.living=true;
+ player.visable=true;
+window.screen10=true;
+ window.color1=("#ffad47");
+this.respawn=(false);
+}
+
+draw(){
+ if(window.screen10==(true)){
+ sky.draw(this.ctx);
+ ground.draw(this.ctx);
+   player.draw(this.ctx);
+}
+}
+
+erraseCanvas() {
+ this.ctx.clearRect(0,0,1000,500);
+}
+}
---
+++ screen9.js
@@ -0,0 +1,29 @@
+export default class Screen9 {
+    constructor() {
+        this.width = 50;
+        this.height = 500;
+        this.position = { x: 950, y: 0 };
+        window.screen10 = (false);
+        this.move = (true);
+        this.redenemy1dead = false;
+    }
+    draw(ctx) {
+        ctx.fillStyle = ("#87CEEB");
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+    proccessloop() {
+        if (window.screen9 == (true)) {
+            if ((window.PlayerX + 100) > this.position.x) {
+                if ((this.redenemy1dead == true) && (this.redenemy2dead == true)) {
+                    if (window.screenload == false) {
+                        window.screen9 = (false);
+                        window.screen10 = (true);
+                        this.move = (false);
+                        setTimeout(function (load2) { load2.move = (true); }, 34, this);
+                    }
+                }
+            }
+        }
+        window.move = this.move;
+    }
+}
