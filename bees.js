fb69af56d8dc0631d7de25f14ad620a5c89c8d0a
websiteforrubikscube <mineyuri77@gmail.com>
2020-05-12 11:30:59 -0400

    k

--- bees.js
+++ bees.js
@@ -1,20 +1,24 @@
+const TotalBees=20;
+
 export default class Bees{
+    
 constructor(){
+
     this.beesx=[];
     this.beesy=[]
-    for(let i =0; i<21;i++){
+    for(let i =0; i<TotalBees;i++){
 this.beesx[i]=(Math.round(Math.random()*1000));
 this.beesy[i]=(Math.round(Math.random()*100));
     }
 }
 draw(ctx){
-    for(let x=0;x<21;x++){
+    for(let x=0;x<TotalBees;x++){
         ctx.fillStyle=("#000");
         ctx.fillRect(this.beesx[x],this.beesy[x],3,3)
     }
 }
 proccess(){
-    for(let y=0;y<21;y++){
+    for(let y=0;y<TotalBees;y++){
         if(this.beesx[y]<950){
         this.beesx[y]=this.beesx[y]+Math.round(Math.random()*5);
         }
@@ -29,17 +33,20 @@ proccess(){
         }
 }
 }
+
 iskillplayer(){
-for(let z=0;z<21;z++){
-    if(this.beesx[z]-3>window.PlayerX){
-        if(this.beesx[z]<window.PlayerX+100){
-            if(this.beesy[z]+3>window.PlayerY){
-                if(this.beesy[z]<window.PlayerY+150){
-return true;
-                }else{return false;}
-            }else{return false;}
-        }else{return false;}
-    }else{return false;}
+    for(let z=0;z<TotalBees;z++){
+        if(this.beesx[z]-3>window.PlayerX){
+            if(this.beesx[z]<window.PlayerX+100){
+                if(this.beesy[z]+3>window.PlayerY){
+                    if(this.beesy[z]<window.PlayerY+150){
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
 }
-}
+
 }
