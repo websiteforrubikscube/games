52fffdecd8709a886d46875d7d6d00e089ef898b
websiteforrubikscube <mineyuri77@gmail.com>
2020-05-23 11:03:11 -0400

    k

--- level3.js
+++ level3.js
@@ -122,7 +122,6 @@ export default class Level3{
       enemyyellow2.proccessloop(this.ctx);
       this.redenemy1.proccess(this.ctx);
       this.redenemy2.proccess(this.ctx);
-  obstacle2.proccessloop();
   if(runnumber23>2){ 
     screen8.proccessloop();
     }
